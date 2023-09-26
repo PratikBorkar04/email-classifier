f416e91c7800291ee23ff9b3a9d8c531c72b2e3d
Pratik Borkar <pratikab01@gmail.com>
2023-09-27 03:16:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if st.button('Predict'):
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
+    vector_input = model.transform([transformed_sms])
     # 3. predict
     result = model.predict(vector_input)[0]
     # 4. Display
