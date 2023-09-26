ff451b796143bcafa1881bfc2c87d677562f61f9
Pratik Borkar <pratikab01@gmail.com>
2023-09-27 03:15:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('corpus')
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
