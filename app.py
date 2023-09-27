ee0a32e40c8a0cd14cb3840c5a2140729f349a83
Pratik Borkar <pratikab01@gmail.com>
2023-09-27 12:15:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,23 +38,23 @@ tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('AB-model.pkl','rb'))
 
 # Set page title and header
-st.set_page_config(page_title="Spam Classifier", page_icon=":shield:", layout="centered")
+st.set_page_config(page_title="Email Classifier", page_icon=":shield:", layout="centered")
 
 # Create a header
-st.title("Email/SMS Spam Classifier")
+st.title("Email Classifier")
 
 # Input for user text
-input_sms = st.text_area("Enter the message", "Type your message here...")
+input_email = st.text_area("Enter the email", "Type your email content here...")
 
 # Predict button
 if st.button('Predict'):
     # Check if input is empty
-    if not input_sms.strip():
-        st.warning("Please enter a message for prediction.")
+    if not input_email.strip():
+        st.warning("Please enter an email for classification.")
     else:
         # Preprocess and predict
-        transformed_sms = transform_text(input_sms)
-        vector_input = tfidf.transform([transformed_sms])
+        transformed_email = transform_text(input_email)
+        vector_input = tfidf.transform([transformed_email])
         result = model.predict(vector_input)[0]
 
         # Display result
